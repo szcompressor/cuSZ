ac8f3debb1842b81f8d16f14e004fc91541dcbd2
J. Tian <tian.jn09@gmail.com>
2020-10-26 21:32:09 -0500

    update copyright
    Former-commit-id: 9278f47718d8112dbd4b66ddc06d0862dcffe438

--- src/gather_scatter.cuh
+++ src/gather_scatter.cuh
@@ -12,7 +12,7 @@
  * @date 2020-09-20
  * Created on 2020-09-10
  *
- * @copyright Copyright (c) 2020 by Washington State University, The University of Alabama, Argonne National Laboratory
+ * @copyright (C) 2020 by Washington State University, Argonne National Laboratory
  * See LICENSE in top-level directory
  *
  */
