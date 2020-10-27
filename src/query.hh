95d8b3fd34df131576c424259c552f8b13393c70
J. Tian <tian.jn09@gmail.com>
2020-10-26 21:05:15 -0500

    add machine query and --verbose
    Former-commit-id: a563ebd1d8aaf42b911e83f06569c47bd77f2192

--- src/query.hh
+++ src/query.hh
@@ -21,6 +21,8 @@
 #include <string>
 #include <vector>
 
+#include <cuda_runtime.h>
+
 using namespace std;
 using std::cerr;
 using std::cout;
@@ -37,9 +39,43 @@ std::string ExecShellCommand(const char* cmd)
     return result;
 }
 
-std::vector<std::string> GetMachineProperties()
+void GetDeviceProperty()
+{
+    int         num_dev  = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&num_dev);
+
+    if (error_id != cudaSuccess) {
+        printf("cudaGetDeviceCount returned %d\n-> %s\n", static_cast<int>(error_id), cudaGetErrorString(error_id));
+        exit(EXIT_FAILURE);
+    }
+    if (num_dev == 0) { printf("NO CUDA device detected.\n"); }
+    int dev, driver_ver = 0, runtime_ver = 0;
+
+    for (dev = 0; dev < num_dev; ++dev) {
+        cudaSetDevice(dev);
+        cudaDeviceProp dev_prop;
+        cudaGetDeviceProperties(&dev_prop, dev);
+        printf("device #%d, %s: \n", dev, dev_prop.name);
+
+        cudaDriverGetVersion(&driver_ver);
+        cudaRuntimeGetVersion(&runtime_ver);
+        printf(
+            "  driver/runtime\t%d.%d/%d.%d\n", driver_ver / 1000, (driver_ver % 100) / 10, runtime_ver / 1000,
+            (runtime_ver % 100) / 10);
+        printf("  compute capability:\t%d.%d\n", dev_prop.major, dev_prop.minor);
+        printf("  global memory:\t%.0f MiB\n", static_cast<float>(dev_prop.totalGlobalMem / 1048576.0f));
+        printf("  constant memory:\t%zu bytes\n", dev_prop.totalConstMem);
+        printf("  shared mem per block:\t%zu bytes\n", dev_prop.sharedMemPerBlock);
+        printf("  shared mem per SM:\t%zu bytes\n", dev_prop.sharedMemPerMultiprocessor);
+        printf("  registers per block:\t%d\n", dev_prop.regsPerBlock);
+    }
+    printf("\n");
+}
+
+void GetMachineProperties()
 {
     std::vector<std::string> v;
+    cout << "host informaton: " << endl;
 
     auto cpuinfo = ExecShellCommand(  //
         std::string("cat /proc/cpuinfo "
@@ -47,7 +83,7 @@ std::vector<std::string> GetMachineProperties()
                     "| head -n 1 "
                     "| awk -F': ' '{print $NF}'")
             .c_str());
-    cout << "cpu model\t" << cpuinfo;
+    cout << "  cpu model\t" << cpuinfo;
 
     auto meminfo = ExecShellCommand(  //
         std::string("cat /proc/meminfo"
@@ -55,7 +91,7 @@ std::vector<std::string> GetMachineProperties()
                     "| awk -F' ' '{print $2\" \"$3}'")
             .c_str());
 
-    cout << "memory size\t" << meminfo;
+    cout << "  memory size\t" << meminfo;
 
     auto endianness = ExecShellCommand(  //
         std::string("lscpu "
@@ -63,8 +99,8 @@ std::vector<std::string> GetMachineProperties()
                     "| awk -F'  ' '{print $NF}'")
             .c_str());
 
-    cout << "byte order\t" << endianness;
-    return v;
+    cout << "  byte order\t" << endianness;
+    printf("\n");
 }
 
 #endif
