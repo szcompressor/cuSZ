d76a23122db2a937e0c7298438062b9323f5f986
J. Tian <tian.jn09+dev@gmail.com>
2021-04-26 11:52:09 -0500

    [optim] create dummy xlorenzo program
    Former-commit-id: 2142ea59461f73c3c525040ee75f1a92d1378985

---
+++ test/dummy_xlorenzo3d.cu
@@ -0,0 +1,36 @@
+
+#include "../src/kernel/lorenzo.cuh"
+#include "../src/metadata.hh"
+
+using Data  = float;
+using Quant = unsigned short;
+
+int main()
+{
+    LorenzoUnzipContext ctx;
+    ctx.d0      = 512;
+    ctx.d1      = 512;
+    ctx.d2      = 512;
+    ctx.stride1 = 512;
+    ctx.stride2 = 512 * 512;
+    ctx.radius  = 0;
+    ctx.ebx2    = 1;
+
+    auto length = ctx.d0 * ctx.d1 * ctx.d2;
+
+    union {
+        Data* data;
+        Data* outlier;
+    } array;
+    unsigned short* quant;
+
+    cudaMallocManaged(&array.data, length * sizeof(Data));
+    cudaMallocManaged(&quant, length * sizeof(Quant));
+
+    kernel::x_lorenzo_3d1l_8x8x8_v3<Data, Quant>
+        <<<dim3(ctx.d0 / 8, ctx.d1 / 8, ctx.d2 / 8), dim3(8, 1, 8)>>>(ctx, array.data, array.outlier, quant);
+
+    cudaDeviceSynchronize();
+
+    return 0;
+}
