f0ccd1c821f7bd2dfa446842cbc027876f25f781
J. Tian <tian.jn09+dev@gmail.com>
2021-06-14 10:14:55 -0500

    [refactor] modify huffman codec

--- src/kernel/hist.h
+++ src/kernel/hist.h
@@ -1,5 +1,5 @@
 /**
- * @file histogram.cuh
+ * @file hist.h
  * @author Cody Rivera (cjrivera1@crimson.ua.edu), Megan Hickman Fulp (mlhickm@g.clemson.edu)
  * @brief Fast histogramming from [Gómez-Luna et al. 2013]
  * @version 0.1
@@ -11,8 +11,8 @@
  *
  */
 
-#ifndef KERNEL_HIST_CUH
-#define KERNEL_HIST_CUH
+#ifndef CUSZ_KERNEL_HIST_H
+#define CUSZ_KERNEL_HIST_H
 
 #include <cuda_runtime.h>
 #include <cstdio>
