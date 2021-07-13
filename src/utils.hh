61efdc6eef55ae71db41143fc613b110db4b0ab4
J. Tian <tian.jn09+dev@gmail.com>
2021-07-12 23:52:28 -0500

    [hotfix] add header of all utils

---
+++ src/utils.hh
@@ -0,0 +1,22 @@
+/**
+ * @file utils.hh
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.3
+ * @date 2021-07-12
+ *
+ * (C) 2021 by Washington State University, Argonne National Laboratory
+ *
+ */
+
+#ifndef UTILS_HH
+#define UTILS_HH
+
+#include "utils/cuda_err.cuh"
+#include "utils/cuda_mem.cuh"
+#include "utils/format.hh"
+#include "utils/io.hh"
+#include "utils/timer.hh"
+#include "utils/verify.hh"
+
+#endif
