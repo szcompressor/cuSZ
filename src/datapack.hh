d5c51cdf92b1b9f6402f6dabc7052b4f48a52e3d
J. Tian <tian.jn09+dev@gmail.com>
2021-08-02 00:08:09 -0500

    [draft] update data container

--- src/datapack.hh
+++ src/datapack.hh
@@ -20,18 +20,88 @@
 #define CONSTEXPR
 #endif
 
+#include <cuda_runtime.h>
+#include <stdexcept>
+
+enum class MODE { TEST, RELEASE };
+enum class WHERE { HOST, DEVICE, BOTH };
+
 template <typename T>
 struct PartialData {
     using type = T;
+    unsigned int len;
     T*           dptr;
     T*           hptr;
-    unsigned int len;
-    unsigned int nbyte() const { return len * sizeof(T); };
 
-    PartialData(unsigned int _len = 0) { len = _len; }
-    void memset(unsigned char init = 0x0u) { cudaMemset(dptr, init, nbyte()); }
-    void h2d() { cudaMemcpy(dptr, hptr, nbyte(), cudaMemcpyHostToDevice); }
-    void d2h() { cudaMemcpy(hptr, dptr, nbyte(), cudaMemcpyDeviceToHost); }
+    PartialData() = default;
+
+    PartialData(unsigned int _len) : len(_len) {}
+
+    unsigned int nbyte() const { return len * sizeof(T); }
+
+    // TODO really useful?
+    struct PartialData& memset(unsigned char init = 0x0u)
+    {
+        cudaMemset(dptr, init, nbyte());
+        return *this;
+    }
+
+    struct PartialData& h2d()
+    {
+        cudaMemcpy(dptr, hptr, nbyte(), cudaMemcpyHostToDevice);
+        return *this;
+    }
+    struct PartialData& d2h()
+    {
+        cudaMemcpy(hptr, dptr, nbyte(), cudaMemcpyDeviceToHost);
+        return *this;
+    }
+    template <MODE m, WHERE w>
+    struct PartialData& alloc()
+    {
+        if (m != MODE::TEST) throw std::runtime_error("Impromptu allocation should only used when MODE::TEST.");
+
+        if (w == WHERE::HOST) {
+            cudaMallocHost(&hptr, nbyte());
+            cudaMemset(hptr, 0x00, nbyte());
+        }
+        else if (w == WHERE::DEVICE) {
+            cudaMalloc(&dptr, nbyte());
+            cudaMemset(dptr, 0x00, nbyte());
+        }
+        else if (w == WHERE::BOTH) {
+            cudaMallocHost(&hptr, nbyte());
+            cudaMemset(hptr, 0x00, nbyte());
+            cudaMalloc(&dptr, nbyte());
+            cudaMemset(dptr, 0x00, nbyte());
+        }
+        else {
+            throw std::runtime_error("Just ran into somewhere strange; not supposed to be here.");
+        }
+
+        return *this;
+    }
+    template <MODE m, WHERE w>
+    struct PartialData& free()
+    {
+        if (m != MODE::TEST) throw std::runtime_error("Impromptu allocation should only used when MODE::TEST.");
+
+        if (w == WHERE::HOST) {  //
+            cudaFreeHost(hptr);
+        }
+        else if (w == WHERE::DEVICE) {
+            cudaFree(dptr);
+        }
+        else if (w == WHERE::BOTH) {
+            cudaFreeHost(hptr);
+            cudaFree(dptr);
+        }
+        else {
+            throw std::runtime_error("Just ran into somewhere strange; not supposed to be here.");
+        }
+
+        return *this;
+    }
 };
 
 #endif
