1086bac862d0fbfd87943b880f917026e34ff7a2
J. Tian <tian.jn09@gmail.com>
2021-09-28 17:07:37 +0000

    create predictor-select paths (unfinished)

--- src/cusz.cu
+++ src/cusz.cu
@@ -37,7 +37,7 @@ using std::string;
 
 namespace {
 
-template <typename Data>
+template <typename T>
 void check_shell_calls(string cmd_string)
 {
     char* cmd = new char[cmd_string.length() + 1];
@@ -50,8 +50,8 @@ void check_shell_calls(string cmd_string)
 
 }  // namespace
 
-template <typename Data, int DownscaleFactor, int tBLK>
-Data* pre_binning(Data* d, size_t* dim_array)
+template <typename T, int DownscaleFactor, int tBLK>
+T* pre_binning(T* d, size_t* dim_array)
 {
     throw std::runtime_error("[pre_binning] disabled temporarily, will be part of preprocessing.");
     return nullptr;
@@ -59,24 +59,19 @@ Data* pre_binning(Data* d, size_t* dim_array)
 
 #define NONPTR_TYPE(VAR) std::remove_pointer<decltype(VAR)>::type
 
-int main(int argc, char** argv)
+void normal_path_lorenzo(cuszCTX* ctx)
 {
-    auto ctx = new cuszCTX(argc, argv);
-
-    if (ctx->verbose) {
-        GetMachineProperties();
-        GetDeviceProperty();
-    }
-
     // TODO remove hardcode for float for now
-    using Data = float;
+    using T = float;
+    using E = QuantTrait<2>::type;
+    using P = FastLowPrecisionTrait<true>::type;
 
     // TODO align to 128 unconditionally
     auto len = ctx->data_len;
     auto m   = Reinterpret1DTo2D::get_square_size(len);
     auto mxm = m * m;
 
-    Capsule<Data> in_data(mxm);
+    Capsule<T> in_data(mxm);
 
     if (ctx->task_is.construct or ctx->task_is.experiment or ctx->task_is.dryrun) {
         cudaMalloc(&in_data.dptr, in_data.nbyte());
@@ -84,20 +79,20 @@ int main(int argc, char** argv)
 
         {
             auto a = hires::now();
-            io::read_binary_to_array<Data>(ctx->fnames.path2file, in_data.hptr, len);
+            io::read_binary_to_array<T>(ctx->fnames.path2file, in_data.hptr, len);
             auto z = hires::now();
 
             if (ctx->verbose) LOGGING(LOG_DBG, "time loading datum:", static_cast<duration_t>(z - a).count(), "sec");
 
-            LOGGING(LOG_INFO, "load", ctx->fnames.path2file, len * sizeof(Data), "bytes");
+            LOGGING(LOG_INFO, "load", ctx->fnames.path2file, len * sizeof(T), "bytes");
         }
 
         in_data.h2d();
 
         if (ctx->mode == "r2r") {
             // TODO prescan can be issued independently from "r2r"
-            auto result = Analyzer::get_maxmin_rng                            //
-                <Data, ExecutionPolicy::cuda_device, AnalyzerMethod::thrust>  //
+            auto result = Analyzer::get_maxmin_rng                         //
+                <T, ExecutionPolicy::cuda_device, AnalyzerMethod::thrust>  //
                 (in_data.dptr, len);
             if (ctx->verbose) LOGGING(LOG_DBG, "time scanning:", result.seconds, "sec");
             if (ctx->mode == "r2r") ctx->eb *= result.rng;
@@ -120,11 +115,11 @@ int main(int argc, char** argv)
     if (ctx->task_is.construct or ctx->task_is.dryrun) {  // fp32 only for now
 
         if (ctx->huff_nbyte == 4) {
-            Compressor<Data, QuantTrait<2>::type, HuffTrait<4>::type, float> cuszc(ctx, cusz::WHEN::COMPRESS);
+            Compressor<T, E, HuffTrait<4>::type, P> cuszc(ctx, cusz::WHEN::COMPRESS);
             cuszc.compress(&in_data);
         }
         else {
-            Compressor<Data, QuantTrait<2>::type, HuffTrait<8>::type, float> cuszc(ctx, cusz::WHEN::COMPRESS);
+            Compressor<T, E, HuffTrait<8>::type, P> cuszc(ctx, cusz::WHEN::COMPRESS);
             cuszc.compress(&in_data);
         }
 
@@ -141,13 +136,31 @@ int main(int argc, char** argv)
         // TODO data ready outside Decompressor?
 
         if (ctx->huff_nbyte == 4) {
-            Compressor<Data, QuantTrait<2>::type, HuffTrait<4>::type, float> cuszd(ctx, cusz::WHEN::DECOMPRESS);
+            Compressor<T, E, HuffTrait<4>::type, P> cuszd(ctx, cusz::WHEN::DECOMPRESS);
             cuszd.decompress();
         }
         else if (ctx->huff_nbyte == 8) {
-            Compressor<Data, QuantTrait<2>::type, HuffTrait<8>::type, float> cuszd(ctx, cusz::WHEN::DECOMPRESS);
+            Compressor<T, E, HuffTrait<8>::type, P> cuszd(ctx, cusz::WHEN::DECOMPRESS);
             cuszd.decompress();
         }
         // }
     }
+}
+
+void special_path_spline3(cuszCTX* ctx)
+{
+    //
+}
+
+int main(int argc, char** argv)
+{
+    auto ctx = new cuszCTX(argc, argv);
+
+    if (ctx->verbose) {
+        GetMachineProperties();
+        GetDeviceProperty();
+    }
+
+    if (ctx->predictor == "lorenzo") normal_path_lorenzo(ctx);
+    if (ctx->predictor == "spline3") special_path_spline3(ctx);
 }
