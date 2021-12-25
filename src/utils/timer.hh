e04baa94e8cd7ac439312506390621b496a96686
J. Tian <tian.jn09+dev@gmail.com>
2021-12-25 03:13:06 +0000

    update timer
    - timer for device: use CUDA event API
    - timer for host: add new methods

--- src/utils/timer.hh
+++ src/utils/timer.hh
@@ -4,7 +4,7 @@
  * @brief High-resolution timer wrapper from <chrono> and util functions for timing both CPU and CUDA function
  * @version 0.2
  * @date 2021-01-05
- * Created on 2019-08-26
+ * (created) 2019-08-26 (rev) 2021-12-23
  *
  * @copyright (C) 2020 by Washington State University, The University of Alabama, Argonne National Laboratory
  * See LICENSE in top-level directory
@@ -20,6 +20,15 @@
 using hires         = std::chrono::high_resolution_clock;
 using duration_t    = std::chrono::duration<double>;
 using hires_clock_t = std::chrono::time_point<hires>;
+
+typedef struct Timer {
+    hires_clock_t start, end;
+
+    void   timer_start() { start = hires::now(); }
+    void   timer_end() { end = hires::now(); }
+    double get_time_elapsed() { return static_cast<duration_t>(end - start).count(); }
+
+} host_timer_t;
 
 #ifdef __CUDACC__
 
@@ -80,7 +89,7 @@ typedef struct CUDATimer {
  * @return double time in seconds
  */
 template <typename F, typename... Args>
-double TimeThisFunction(F func, Args&&... args)
+double TimeThisRoutine(F func, Args&&... args)
 {
     auto t0 = hires::now();
     func(std::forward<Args>(args)...);
@@ -91,7 +100,7 @@ double TimeThisFunction(F func, Args&&... args)
 typedef struct CUDAKernelConfig {
     dim3         dim_grid;
     dim3         dim_block;
-    size_t       num_shmem_bytes{0};
+    size_t       shmem_nbyte{0};
     cudaStream_t stream;
 
 } kernelcfg;
@@ -108,15 +117,26 @@ typedef struct CUDAKernelConfig {
  * @return double time in seconds
  */
 template <typename F, typename... Args>
-double TimeThisCUDAFunction(F func, kernelcfg cfg, Args&&... args)
+float TimeThisCUDARoutine(F func, kernelcfg cfg, Args&&... args)
 {
-    auto t0 = hires::now();
-    func<<<cfg.dim_grid, cfg.dim_block, cfg.num_shmem_bytes, cfg.stream>>>(  //
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    func<<<cfg.dim_grid, cfg.dim_block, cfg.shmem_nbyte, cfg.stream>>>(  //
         args...
         // std::forward<Args>(args)... // also works
     );
-    cudaDeviceSynchronize();
-    return static_cast<duration_t>(hires::now() - t0).count();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaStreamSynchronize(cfg.stream);
+
+    float milliseconds;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    return milliseconds;
 }
 
 #endif
--- test/test_timer.cu
+++ test/test_timer.cu
@@ -5,10 +5,17 @@
 
 using namespace std;
 
+class A {
+   public:
+    A() = default;
+    static void f() {}
+    static void g() {}
+};
+
 __global__ void df(double a, double b)
 {
     //
-    printf("test device, %u\n", threadIdx.x);
+    printf("test device; thread id: %u\n", threadIdx.x);
 }
 
 void hf(double a, double b)
@@ -17,13 +24,35 @@ void hf(double a, double b)
     printf("test host\n");
 }
 
+void hf2()
+{
+    //
+    printf("test host\n");
+}
+
 int main()
 {
-    auto t1 = TimeThisFunction(hf, 2.0, 3.0);
+    auto t1 = TimeThisRoutine(hf, 2.0, 3.0);
     cout << t1 << endl;
 
-    auto t2 = TimeThisCUDAFunction(df, kernelcfg{dim3(1), dim3(64), 0, nullptr}, 2.0, 3.0);
+    t1 = TimeThisRoutine(hf2);
+    cout << t1 << endl;
+
+    auto t2 = TimeThisCUDARoutine(df, kernelcfg{dim3(1), dim3(64), 0, nullptr}, 2.0, 3.0);
     cout << t2 << endl;
+
+    A a;
+
+    host_timer_t timer;
+    timer.timer_start();
+    a.f();
+    timer.timer_end();
+    cout << timer.get_time_elapsed() << endl;
+
+    timer.timer_start();
+    A::g();
+    timer.timer_end();
+    cout << timer.get_time_elapsed() << endl;
 
     return 0;
 }
