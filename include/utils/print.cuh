2669f9c4f7fc9c03ff65dea7429c7af6865d36ca
J. Tian <tian.jn09+dev@gmail.com>
2022-09-23 23:19:21 +0000

    add print aux functions

---
+++ include/utils/print.cuh
@@ -0,0 +1,50 @@
+/**
+ * @file print.cuh
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.3
+ * @date 2022-09-23
+ *
+ * (C) 2022 by Indiana University, Argonne National Laboratory
+ *
+ */
+
+#ifndef D79937FC_16EB_413C_AE31_27A8AACCCF46
+#define D79937FC_16EB_413C_AE31_27A8AACCCF46
+
+template <typename T>
+void __host__ __device__ typed_printf(const T val);
+
+template <>
+void typed_printf<uint32_t>(const uint32_t i)
+{
+    printf("%u\t", i);
+}
+
+template <>
+void typed_printf<uint16_t>(const uint16_t i)
+{
+    printf("%u\t", (uint32_t)i);
+}
+
+template <>
+void typed_printf<uint8_t>(const uint8_t i)
+{
+    printf("%u\t", (uint32_t)i);
+}
+
+template <>
+void typed_printf<float>(const float i)
+{
+    printf("%.7f\t", i);
+}
+
+/* code snippet for looking at the device array easily */
+template <typename T>
+void __host__ __device__ peek_device_data(T* d_arr, size_t num, size_t offset = 0)
+{
+    thrust::for_each(thrust::device, d_arr, d_arr + num, [=] __device__ __host__(const T i) { typed_printf<T>(i); });
+    printf("\n");
+};
+
+#endif /* D79937FC_16EB_413C_AE31_27A8AACCCF46 */
