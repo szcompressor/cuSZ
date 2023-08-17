2ed923c4d284f66ed72b46347e23a7e9b4506c17
J. Tian <tian.jn09+dev@gmail.com>
2023-08-17 09:32:20 +0000

    separate file for `hf_word` definition
    - [TODO] change field size given the word bits

--- include/hf/hf_codecg.hh
+++ include/hf/hf_codecg.hh
@@ -12,52 +12,26 @@
 #ifndef ABAACE49_2C9E_4E3C_AEFF_B016276142E1
 #define ABAACE49_2C9E_4E3C_AEFF_B016276142E1
 
-#include <stdint.h>
-#include <stdlib.h>
-
 #include <cuda_runtime.h>
+#include <cstdint>
+#include <cstdlib>
+
 #include "hf_struct.h"
-
-template <int WIDTH>
-struct PackedWordByWidth;
-
-template <>
-struct PackedWordByWidth<4> {
-    uint32_t word : 24;
-    uint32_t bits : 8;
-};
-
-template <>
-struct PackedWordByWidth<8> {
-    uint64_t word : 56;
-    uint64_t bits : 8;
-};
+#include "hf_word.hh"
 
 namespace psz {
 
 template <typename T, typename H, typename M>
 void hf_encode_coarse_rev2(
-    T*            uncompressed,
-    size_t const  len,
-    hf_book*      book_desc,
-    hf_bitstream* bitstream_desc,
-    size_t*       outlen_nbit,
-    size_t*       outlen_ncell,
-    float*        time_lossless,
-    cudaStream_t  stream);
+    T* uncompressed, size_t const len, hf_book* book_desc,
+    hf_bitstream* bitstream_desc, size_t* outlen_nbit, size_t* outlen_ncell,
+    float* time_lossless, cudaStream_t stream);
 
 template <typename T, typename H, typename M>
 void hf_decode_coarse(
-    H*           d_bitstream,
-    uint8_t*     d_revbook,
-    int const    revbook_nbyte,
-    M*           d_par_nbit,
-    M*           d_par_entry,
-    int const    sublen,
-    int const    pardeg,
-    T*           out_decompressed,
-    float*       time_lossless,
-    cudaStream_t stream);
+    H* d_bitstream, uint8_t* d_revbook, int const revbook_nbyte, M* d_par_nbit,
+    M* d_par_entry, int const sublen, int const pardeg, T* out_decompressed,
+    float* time_lossless, cudaStream_t stream);
 
 }  // namespace psz
 
---
+++ include/hf/hf_word.hh
@@ -0,0 +1,37 @@
+/**
+ * @file hf_word.hh
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.4
+ * @date 2023-08-16
+ *
+ * (C) 2023 by Indiana University, Argonne National Laboratory
+ *
+ */
+
+#ifndef FDF0862D_B4A6_4E38_A11B_7299C37315A9
+#define FDF0862D_B4A6_4E38_A11B_7299C37315A9
+
+#include <cstdint>
+
+template <int WIDTH>
+struct PackedWordByWidth;
+
+// TODO it does not pass 5 and 6 bits, respectively.
+template <>
+struct PackedWordByWidth<4> {
+  static const int field_word = 24;
+  static const int field_bits = 8;
+  uint32_t word : 24;
+  uint32_t bits : 8;
+};
+
+template <>
+struct PackedWordByWidth<8> {
+  static const int field_word = 56;
+  static const int field_bits = 8;
+  uint64_t word : 56;
+  uint64_t bits : 8;
+};
+
+#endif /* FDF0862D_B4A6_4E38_A11B_7299C37315A9 */
