a74f1bee6ce96737ee23ed478df134685c4056f3
J. Tian <tian.jn09+dev@gmail.com>
2023-08-17 07:43:53 +0000

    too busy to import headers every time

---
+++ include/busyheader.h
@@ -0,0 +1,32 @@
+/**
+ * @file busyheader.h
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.4
+ * @date 2023-08-17
+ *
+ * (C) 2023 by Indiana University, Argonne National Laboratory
+ *
+ */
+
+#ifndef DAFECEBC_BF3D_437B_8509_6FFC5CC06579
+#define DAFECEBC_BF3D_437B_8509_6FFC5CC06579
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DAFECEBC_BF3D_437B_8509_6FFC5CC06579 */
---
+++ include/busyheader.hh
@@ -0,0 +1,48 @@
+/**
+ * @file busyheader.hh
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.4
+ * @date 2023-08-17
+ *
+ * (C) 2023 by Indiana University, Argonne National Laboratory
+ *
+ */
+
+#ifndef A4E54262_00A2_4059_BD74_95050FB447CA
+#define A4E54262_00A2_4059_BD74_95050FB447CA
+
+#include <algorithm>
+#include <bitset>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+#include <vector>
+
+using std::bitset;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::runtime_error;
+using std::string;
+using std::stringstream;
+using std::to_string;
+using std::vector;
+
+// C headers in Cxx
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#endif /* A4E54262_00A2_4059_BD74_95050FB447CA */
