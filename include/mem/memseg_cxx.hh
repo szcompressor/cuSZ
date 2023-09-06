763364df6b07523923d8976a86ada2f7b096bbf9
J. Tian <tian.jn09+dev@gmail.com>
2023-09-06 02:18:52 +0000

    add alias of `pszmemseg_cxx<T>`

--- include/mem/memseg_cxx.hh
+++ include/mem/memseg_cxx.hh
@@ -13,6 +13,7 @@
 #ifndef B0F9D525_8AEF_4F79_A668_6C36D73DDF9F
 #define B0F9D525_8AEF_4F79_A668_6C36D73DDF9F
 
+#include <cstddef>
 #include "busyheader.hh"
 #include "cusz/type.h"
 #include "memseg.h"
@@ -47,5 +48,18 @@ using pszmem_control = pszmem_control_stream;
   // #include "memseg_cxx_1api.hh"
 #endif
 
+typedef pszmem_cxx<u1> MemU1;
+typedef pszmem_cxx<u2> MemU2;
+typedef pszmem_cxx<u4> MemU4;
+typedef pszmem_cxx<u8> MemU8;
+typedef pszmem_cxx<ull> MemUll;
+typedef pszmem_cxx<i1> MemI1;
+typedef pszmem_cxx<i2> MemI2;
+typedef pszmem_cxx<i4> MemI4;
+typedef pszmem_cxx<i8> MemI8;
+typedef pszmem_cxx<szt> MemSzt;
+typedef pszmem_cxx<szt> MemZu;
+typedef pszmem_cxx<f4> MemF4;
+typedef pszmem_cxx<f8> MemF8;
 
 #endif /* B0F9D525_8AEF_4F79_A668_6C36D73DDF9F */
