48c16faa2e27eb29f11d16619b4bef6fd3949792
J. Tian <tian.jn09+dev@gmail.com>
2025-02-15 08:11:08 +0000

    [REBASE] add utils to generate bk and revbk

--- example/cmake/cuda-example.cmake
+++ example/cmake/cuda-example.cmake
@@ -24,3 +24,6 @@ target_link_libraries(bin_fzgcodec PRIVATE cusz CUDA::cudart)
 
 add_executable(batch_run src/batch_run.cc)
 target_link_libraries(batch_run PRIVATE cusz example_utils2 CUDA::cudart)
+
+add_executable(pbk_hist2book src/pbk_hist2book.cc)
+target_link_libraries(pbk_hist2book PRIVATE cusz CUDA::cudart)
